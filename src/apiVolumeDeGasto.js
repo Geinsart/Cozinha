2afc778fd474662bad0f1d320639c28a3df1474a
Geinsart <georgens.10@gmail.com>
2024-12-14 04:19:03 -0300

    subindo api atualizada

--- src/apiVolumeDeGasto.js
+++ src/apiVolumeDeGasto.js
@@ -1,11 +1,15 @@
-const url = 'https://api.thecatapi.com/v1/images/search?limit=10'
+const url = "http://www.randomnumberapi.com/api/v1.0/randomnumber";
 
 async function getApi() {
     const response = await fetch(url);
-    if (response.status === 200) {
-        const data = await response.json();
-        console.log(data);
+    const data = await response.json();
+    const number = data[0];
+
+    return {
+        number,
     }
 }
 
-chamarApi();
+getApi();
+
+console.log(number + 5)
